53dc04e5b434b3733cc0be24d5276567f1da597e
Alex Palcuie <alex.palcuie@gmail.com>
2017-02-20 20:53:03 +0000

    Our version

--- src/Inn.py
+++ src/Inn.py
@@ -2,9 +2,49 @@
 
 from Item import *
 
+def update_stock_item(item):
+    item.setSellIn(item.getSellIn() - 1)
+
+    if item.getSellIn() < 0:
+        item.setQuality(item.getQuality() - 1)
+
+    item.setQuality(item.getQuality() - 1)
+
+def update_brie_item(item):
+    item.setQuality(item.getQuality() + 1)
+
+def update_backstage_pass_item(item):
+    item.setSellIn(item.getSellIn() - 1)
+
+    if item.getSellIn() < 0:
+        item.setQuality(0)
+    if item.getSellIn() > 10:
+        item.setQuality(item.getQuality() + 1)
+
+def update_item(item):
+    success = False
+    if item.getName() in ["+5 Dexterity Vest", "Elixir of the Mongoose"]:
+        update_stock_item(item)
+        success = True
+    if item.getName() == "Aged Brie":
+        update_brie_item(item)
+        success= True
+    if item.getName() == "Backstage passes to a TAFKAL80ETC concert":
+        update_backstage_pass_item(item)
+        success = True
+
+    if success and item.getQuality() > 50:
+        item.setQuality(50)
+    if success and item.getQuality() < 0:
+        item.setQuality(0)
+
+    return success
+
 class Inn(object):
     def __init__(self):
         self.__itemslist=[]
+
+    def addFixtures(self):
         self.__itemslist.append(Item("+5 Dexterity Vest", 10, 20))
         self.__itemslist.append(Item("Aged Brie", 2, 0))
         self.__itemslist.append(Item("Elixir of the Mongoose", 5, 7))
@@ -12,15 +52,20 @@ class Inn(object):
         self.__itemslist.append(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
         self.__itemslist.append(Item("Conjured Mana Cake", 3, 6))
 
+    def addItem(self, item):
+        self.__itemslist.append(item)
+
     def getList(self):
         return self.__itemslist
 
     def updateQuality(self):
         for item in self.__itemslist:
-            if item.getName()=="Aged Brie" and item.getName()=="Backstage passes to a TAFKL80ETC concert":
-                if item.getQuality()>0:
-                    if item.getName()=="Sulfuras, Hand of Ragnaros":
-                        item.setQuality(item.getQuality()-1)
+            result = update_item(item)
+            if result:
+                continue
+            if item.getQuality()>0:
+                if item.getName()!="Sulfuras, Hand of Ragnaros":
+                    item.setQuality(item.getQuality()-1)
             else:
                 if item.getQuality()<50:
                     item.setQuality(item.getQuality()+1)
@@ -34,7 +79,7 @@ class Inn(object):
                             if item.getQuality()<50:
                                 item.setQuality(item.getQuality()+1)
 
-            if item.getName()=="Sulfuras, Hand of Ragnaros":
+            if item.getName()!="Sulfuras, Hand of Ragnaros":
                 item.setSellIn(item.getSellIn()-1)
 
             if item.getSellIn()<0:
@@ -45,8 +90,8 @@ class Inn(object):
                     else:
                         item.setQuality(item.getQuality()-item.getQuality())
                 else:
-                    if item.getQuality()<50:
-                        item.setQuality(item.getQuality()+1)
+                    if item.getQuality()>0:
+                        item.setQuality(item.getQuality()-1)
 
 def main():
     myapp = Inn()
--- src/InnTest.py
+++ src/InnTest.py
@@ -1,9 +1,82 @@
 from Inn import *
+import unittest
 
 def testInn():
     myinn = Inn()
     assert(True)
 
+#def test_item_value_after_one_day():
+
+class TestInn(unittest.TestCase):
+    def test_update_quality_decreases_sell_in(self):
+        myinn = Inn()
+        myinn.addItem(Item("Elixir of the Mongoose", 10, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getSellIn(), 9)
+
+    def test_update_quality_does_not_decrease_for_sulfuras(self):
+        myinn = Inn()
+        myinn.addItem(Item("Sulfuras, Hand of Ragnaros", 10, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getSellIn(), 10)
+
+    def test_update_quality_decreases_quality(self):
+        myinn = Inn()
+        myinn.addItem(Item("Elixir of the Mongoose", 10, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 19)
+
+    def test_update_quality_does_not_decrease_quality_for_sulfuras(self):
+        myinn = Inn()
+        myinn.addItem(Item("Sulfuras, Hand of Ragnaros", 10, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 20)
+
+    def test_update_quality_decreases_quality_by_two_past_the_sellin_date(self):
+        myinn = Inn()
+        myinn.addItem(Item("Elixir of the Mongoose", -1, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 18)
+
+    def test_update_quality_increases_quality_of_bree_by_two_past_the_sellin_date(self):
+        myinn = Inn()
+        myinn.addItem(Item("Aged Brie", 1, 20))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 21)
+
+    def test_quality_never_goes_over_50(self):
+        myinn = Inn()
+        myinn.addItem(Item("Aged Brie", 1, 50))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 50)
+
+    def test_quality_never_goes_below_0(self):
+        myinn = Inn()
+        myinn.addItem(Item("Elixir of the Mongoose", 1, 0))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 0)
+
+    def test_backstage_goes_to_zero_after_concert(self):
+        myinn = Inn()
+        myinn.addItem(Item("Backstage passes to a TAFKAL80ETC concert", 0, 50))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 0)
+
+    def test_backstage_goes_increases_by_one_more_than_10_days_before_concert(self):
+        myinn = Inn()
+        myinn.addItem(Item("Backstage passes to a TAFKAL80ETC concert", 12, 10))
+        myinn.updateQuality()
+        updated_item = myinn.getList()[0]
+        self.assertEqual(updated_item.getQuality(), 11)
 
 if __name__ == "__main__":
-    testInn()
+    unittest.main()
